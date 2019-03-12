26502b8312b3a2e8ede468dc2dbca3c103ac0b36
Unknown <rbrintont823@live.com>
2019-03-12 15:35:02 -0700

    Code clean up of unused items.  Removed ObjectDetection stuff

--- EWU_TEALS_Draw/EWU_TEALS_Draw/MainForm.cs
+++ EWU_TEALS_Draw/EWU_TEALS_Draw/MainForm.cs
@@ -22,12 +22,8 @@ namespace EWU_TEALS_Draw
     {
         private List<IDisposable> Disposables;
         private VideoCapture VideoCapture;
-        public CascadeClassifier CascadeClassifier = null;
-        
-        private Drawing drawing = new Drawing(1280, 720);
 
-
-        // Resolution Properties
+        #region Resolution Properties
         private const int CameraToUse = 0; // Default Camera: 0
         private const int FPS = 30;
         private const int ActualCameraWidth = 1280;
@@ -38,79 +34,40 @@ namespace EWU_TEALS_Draw
 
         private const int CanvasWidth = DisplayedCameraWidth * 3;
         private const int CanvasHeight = DisplayedCameraHeight * 3;
+        #endregion
 
-
-
-
-        // Thresholding Properties
+        #region Color Threshold Properties
         private IInputArray BlueHsvMin = new ScalarArray(new MCvScalar(100, 150, 100)); // Blue min
         private IInputArray BlueHsvMax = new ScalarArray(new MCvScalar(135, 255, 255)); // Blue max
         private MCvScalar BlueDrawColor = new MCvScalar(255, 135, 135); // Blue draw
         private Point BlueLastPosition;
-       
 
         private IInputArray GreenHsvMin = new ScalarArray(new MCvScalar(65, 75, 100)); // Green min
         private IInputArray GreenHsvMax = new ScalarArray(new MCvScalar(85, 255, 255)); // Green max
         private MCvScalar GreenDrawColor = new MCvScalar(135, 230, 135); // Green draw
         private Point GreenLastPosition;
-
-        private Mat Hsv_image;
-        private Mat ThreshLow_image;
-        private Mat ThreshHigh_image;
-        private Mat Thresh_image;
-
-        
-        // TimeSlicing Properties
-        private int LastTime = DateTime.Now.Millisecond;
-        private int Now;
-        private DateTime NowDateTime;
-        private DateTime LastDateTime = DateTime.Now;
-
+        #endregion
 
         public MainForm()
         {
             InitializeComponent();
-            this.WindowState = FormWindowState.Maximized;
+            WindowState = FormWindowState.Maximized;
+
             Startup();
         }
 
         private void Startup()
         {
-            Disposables = new List<IDisposable>();
-
-            //CascadeClassifier = new CascadeClassifier(@"../../HaarCascades/haarcascade_upperbody.xml");
-            //CascadeClassifier = new CascadeClassifier(@"../../HaarCascades/haarcascade_eye.xml");
-            CascadeClassifier = new CascadeClassifier(@"../../HaarCascades/haarcascade_lefteye_2splits.xml");
-
-            Disposables.Add(CascadeClassifier);
-
+            SetupVideoCapture();
             ImageBox_VideoCapture_Gray.Image = new Image<Bgr, byte>(DisplayedCameraWidth, DisplayedCameraHeight, new Bgr(255, 255, 255));
-
             ImageBox_Drawing.Image = new Image<Bgr, byte>(CanvasWidth, CanvasHeight, new Bgr(255, 255, 255));
             
-            
-            Disposables.Add(ImageBox_VideoCapture_Gray.Image);
-            Disposables.Add(ImageBox_Drawing.Image);
-            Disposables.Add(ImageBox_VideoCapture.Image);
-
-            SetupVideoCapture();
-
-
-
-            Mat color_image = VideoCapture.QueryFrame();
-            Hsv_image = new Mat(color_image.Size, DepthType.Cv8U, 3);
-            ThreshLow_image = new Mat(color_image.Size, DepthType.Cv8U, 1);
-            ThreshHigh_image = new Mat(color_image.Size, DepthType.Cv8U, 1);
-            Thresh_image = new Mat(color_image.Size, DepthType.Cv8U, 1);
-
-
             Application.Idle += ProcessFrame;
         }
 
         private void SetupVideoCapture()
         {
             VideoCapture = new VideoCapture(CameraToUse); // Webcam is 1280x720
-            Disposables.Add(VideoCapture);
 
             VideoCapture.SetCaptureProperty(CapProp.Fps, FPS); // The FPS property doesn't actually work...
             VideoCapture.SetCaptureProperty(CapProp.FrameWidth, DisplayedCameraWidth);  // 1280, 640, 320
@@ -124,22 +81,13 @@ namespace EWU_TEALS_Draw
                 Mat flippedVideoFrame = FlipImage(VideoCapture.QueryFrame());
                 ImageBox_VideoCapture.Image = flippedVideoFrame;
 
-
                 Mat blueThreshImage = DetectColor(flippedVideoFrame, BlueHsvMin, BlueHsvMax, BlueDrawColor, BlueLastPosition, "Blue");
                 Mat greenThreshImage = DetectColor(flippedVideoFrame, GreenHsvMin, GreenHsvMax, GreenDrawColor, GreenLastPosition, "Green");
 
                 Mat combinedImage = new Mat();
                 CvInvoke.Add(blueThreshImage, greenThreshImage, combinedImage);
                 ImageBox_VideoCapture_Gray.Image = combinedImage;
-                //ImageBox_VideoCapture_Gray.Image = blueThreshImage;
             }
-        }
-
-        private Mat FlipImage(Mat inputImage)
-        {
-            Mat flippedImage = new Mat(inputImage.Size, DepthType.Cv8U, inputImage.NumberOfChannels);
-            CvInvoke.Flip(inputImage, flippedImage, FlipType.Horizontal);
-            return flippedImage;
         }
 
         private Mat DetectColor(Mat inputImage, IInputArray hsvThreshMin, IInputArray hsvThreshMax, MCvScalar drawColor, Point thisColorLastPosition, string color)
@@ -172,7 +120,6 @@ namespace EWU_TEALS_Draw
 
                 // Draw on canvas
                 avgPoint = ScaleToCanvas(avgPoint);
-                //MCvScalar color = GetColorBySpeed(avgPoint);
 
                 int width = GetWidthBySpeed(thisColorLastPosition, avgPoint);
                 DrawLineTo(avgPoint, drawColor, thisColorLastPosition, width);
@@ -190,7 +137,7 @@ namespace EWU_TEALS_Draw
                         break;
                 }
             }
-            // If not enough pixels to count as an object, reset LastHandPosition to 0 so it will be 
+            // If not enough pixels to count as an object, reset lastColorPosition to 0 so it will be 
             // updated when we do find it.
             else
             {
@@ -310,71 +257,11 @@ namespace EWU_TEALS_Draw
             }
         }
 
-        private void DetectHand(Mat inputImage)
-        {
-            //color_image._EqualizeHist();
-            //color_image._GammaCorrect(1.2d);
-            var gray_image = GetGrayImage(inputImage);
-
-            Rectangle[] hands = CascadeClassifier.DetectMultiScale(gray_image);
-            Point handCenter = new Point();
-            if (hands.Length > 0)
-            {
-                foreach (Rectangle hand in hands)
-                {
-                    CvInvoke.Rectangle(gray_image, hand, new MCvScalar(240, 140, 0), 2);
-                    handCenter.X = hand.X + (hand.Width / 2);
-                    handCenter.Y = hand.Y + (hand.Height / 2);
-                    //CvInvoke.Circle(gray_image, handCenter, 4, new MCvScalar(255,255,255),2);
-
-                    // Draw center on drawing
-                    //drawing.AddPoint(ImageBox_VideoCapture, handCenter.X, handCenter.Y); // Draw with Drawing.cs
-
-                    //handCenter.X = ImageBox_Drawing.Width - handCenter.X * 2; // scale to drawing size
-                    //handCenter.Y = handCenter.Y * 2;
-                    //CvInvoke.Circle(ImageBox_Drawing.Image, handCenter, 4, new MCvScalar(240,140,0), 2);
-
-                    // Draw without Drawing.cs:
-                    handCenter = ScaleToCanvas(handCenter);
-                    MCvScalar color = new MCvScalar(240, 140, 0);
-                    DrawPoint(handCenter, color);
-                }
-            }
-            else if (hands.Length == 0)
-            {
-                BlueLastPosition.X = 0;
-                BlueLastPosition.Y = 0;
-            }
-            
-
-            drawing.Update(ImageBox_Drawing.Image);
-            ImageBox_VideoCapture_Gray.Image = gray_image;
-            ImageBox_Drawing.Refresh();
-            ImageBox_VideoCapture.Image = inputImage;
-        }
-
-        private void DrawPoint(Point point, MCvScalar color)
-        {
-            if (BlueLastPosition.X != 0 && BlueLastPosition.Y != 0)
-            {
-                CvInvoke.Circle(ImageBox_Drawing.Image, point, 5, color, -1); // -1 indicates filled circle
-                ImageBox_Drawing.Refresh();
-            }
-
-            BlueLastPosition = new Point(point.X, point.Y);
-        }
-
         private IImage GetGrayImage(Mat color_image)
         {
             var image = new Image<Gray, byte>(color_image.Bitmap);
 
             return image;
-        }
-
-        private void DrawLine(Point point1, Point point2, MCvScalar color)
-        {   
-            CvInvoke.Line(ImageBox_VideoCapture_Gray.Image, point1, point2, color, 10, LineType.AntiAlias);
-            ImageBox_VideoCapture_Gray.Refresh();
         }
 
         private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
@@ -393,6 +280,13 @@ namespace EWU_TEALS_Draw
 
                 Disposables = null;
             }
+        }
+
+        private Mat FlipImage(Mat inputImage)
+        {
+            Mat flippedImage = new Mat(inputImage.Size, DepthType.Cv8U, inputImage.NumberOfChannels);
+            CvInvoke.Flip(inputImage, flippedImage, FlipType.Horizontal);
+            return flippedImage;
         }
 
         private void btnPlay_Click(object sender, EventArgs e)
