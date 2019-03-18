de54ccfda1cd9d39e88cf7fc239863503d5c7360
Unknown <rbrintont823@live.com>
2019-03-17 19:02:33 -0700

    Added contour approach to increase reliablility.  Memory management

--- EWU_TEALS_Draw/EWU_TEALS_Draw/MainForm.cs
+++ EWU_TEALS_Draw/EWU_TEALS_Draw/MainForm.cs
@@ -24,7 +24,7 @@ namespace EWU_TEALS_Draw
         private VideoCapture VideoCapture;
 
         #region Resolution Properties
-        private const int CameraToUse = 0; // Default Camera: 0
+        private const int CameraToUse = 1; // Default Camera: 0
         private const int FPS = 30;
         private const int ActualCameraWidth = 1920;
         private const int ActualCameraHeight = 1080;
@@ -35,61 +35,58 @@ namespace EWU_TEALS_Draw
         private int CanvasWidth = (int)Math.Floor(DisplayedCameraWidth * 3.77);
         private int CanvasHeight = (int)Math.Floor(DisplayedCameraHeight * 3.77);
         #endregion
-        
-        #region Color Threshold Properties
-        // Blue
-        private IInputArray BlueHsvMin = new ScalarArray(new MCvScalar(105, 125, 65)); // Blue min
-        private IInputArray BlueHsvMax = new ScalarArray(new MCvScalar(117, 255, 255)); // Blue max
-        private MCvScalar BlueDrawColor = new MCvScalar(255, 140, 85); // Blue draw color
-        private Point BlueLastPosition;
 
-        // Green
-        private IInputArray GreenHsvMin = new ScalarArray(new MCvScalar(60, 100, 20));
-        private IInputArray GreenHsvMax = new ScalarArray(new MCvScalar(90, 255, 255));
-        private MCvScalar GreenDrawColor = new MCvScalar(135, 230, 135);
-        private Point GreenLastPosition;
+        #region Color Threshold Properties
+        // Red Low Threshold
+        private IInputArray RedHsvMin_Low = new ScalarArray(new MCvScalar(0, 50, 100));
+        private IInputArray RedHsvMax_Low = new ScalarArray(new MCvScalar(5, 255, 255));
+        // Red High Threshold
+        private IInputArray RedHsvMin_High = new ScalarArray(new MCvScalar(157, 105, 115));
+        private IInputArray RedHsvMax_High = new ScalarArray(new MCvScalar(180, 255, 255));
+        private MCvScalar RedDrawColor = new MCvScalar(60, 60, 230);
+        private Point RedLastPosition;
+
+        // Orange
+        private IInputArray OrangeHsvMin = new ScalarArray(new MCvScalar(10, 175, 65));
+        private IInputArray OrangeHsvMax = new ScalarArray(new MCvScalar(18, 255, 255));
+        private MCvScalar OrangeDrawColor = new MCvScalar(60, 140, 255);
+        private Point OrangeLastPosition;
 
         // Yellow
-        private IInputArray YellowHsvMin = new ScalarArray(new MCvScalar(20, 50, 150));
+        private IInputArray YellowHsvMin = new ScalarArray(new MCvScalar(25, 50, 120));
         private IInputArray YellowHsvMax = new ScalarArray(new MCvScalar(35, 255, 255));
         private MCvScalar YellowDrawColor = new MCvScalar(100, 240, 240);
         private Point YellowLastPosition;
 
-        // Orange
-        private IInputArray OrangeHsvMin = new ScalarArray(new MCvScalar(12, 175, 65));
-        private IInputArray OrangeHsvMax = new ScalarArray(new MCvScalar(18, 255, 255));
-        private MCvScalar OrangeDrawColor = new MCvScalar(60, 140, 255);
-        private Point OrangeLastPosition;
+        // Green
+        private IInputArray GreenHsvMin = new ScalarArray(new MCvScalar(85, 200, 70));
+        private IInputArray GreenHsvMax = new ScalarArray(new MCvScalar(95, 255, 255));
+        private MCvScalar GreenDrawColor = new MCvScalar(135, 230, 135);
+        private Point GreenLastPosition;
+
+        // Blue
+        private IInputArray BlueHsvMin = new ScalarArray(new MCvScalar(95, 170, 110));
+        private IInputArray BlueHsvMax = new ScalarArray(new MCvScalar(117, 255, 255));
+        private MCvScalar BlueDrawColor = new MCvScalar(255, 140, 85);
+        private Point BlueLastPosition;
 
         // Purple
         private IInputArray PurpleHsvMin = new ScalarArray(new MCvScalar(125, 100, 100));
         private IInputArray PurpleHsvMax = new ScalarArray(new MCvScalar(140, 255, 255));
         private MCvScalar PurpleDrawColor = new MCvScalar(255, 135, 135);
         private Point PurpleLastPosition;
-
-        // Red Lower Threshold
-        private IInputArray RedHsvMin_Low = new ScalarArray(new MCvScalar(0, 50, 100));
-        private IInputArray RedHsvMax_Low = new ScalarArray(new MCvScalar(5, 255, 255));
-        // Red Upper Threshold
-        private IInputArray RedHsvMin_High = new ScalarArray(new MCvScalar(170, 175, 45));
-        private IInputArray RedHsvMax_High = new ScalarArray(new MCvScalar(180, 255, 255));
-
-        private MCvScalar RedDrawColor = new MCvScalar(60, 60, 230);
-        private Point RedLastPosition;
         #endregion
 
         #region Re-used Objects for saving memory
         private Mat HsvImage_Temp = new Mat();
-        public Mat BlueThreshImage_Temp = new Mat();
-        public Mat GreenThreshImage_Temp = new Mat();
-        public Mat YellowThreshImage_Temp = new Mat();
-        public Mat OrangeThreshImage_Temp = new Mat();
-        public Mat PurpleThreshImage_Temp = new Mat();
-        public Mat RedThreshImage_Temp = new Mat();
-        private Mat CombinedThreshImage = new Mat();
-        private Point AveragePoint = new Point();
-        private Mat Points;
-        Queue<Mat> VideoFrames = new Queue<Mat>();
+        private Mat RedThreshImage_Temp = new Mat();
+        private Mat OrangeThreshImage_Temp = new Mat();
+        private Mat YellowThreshImage_Temp = new Mat();
+        private Mat GreenThreshImage_Temp = new Mat();
+        private Mat BlueThreshImage_Temp = new Mat();
+        private Mat PurpleThreshImage_Temp = new Mat();
+
+        Queue<Mat> DisposableQueue = new Queue<Mat>();
         #endregion
 
 
@@ -107,7 +104,6 @@ namespace EWU_TEALS_Draw
         {
             SetupVideoCapture();
             ImageBox_Drawing.Image = new Image<Bgr, byte>(CanvasWidth, CanvasHeight, new Bgr(255, 255, 255));
-            Points = new Mat(VideoCapture.QueryFrame().Size, DepthType.Cv8U, 1);
 
             Application.Idle += ProcessFrame;
             btnPlay.Enabled = false;
@@ -121,6 +117,9 @@ namespace EWU_TEALS_Draw
             VideoCapture.SetCaptureProperty(CapProp.Fps, FPS);
             VideoCapture.SetCaptureProperty(CapProp.FrameWidth, DisplayedCameraWidth);
             VideoCapture.SetCaptureProperty(CapProp.FrameHeight, DisplayedCameraHeight);
+            VideoCapture.SetCaptureProperty(CapProp.Autofocus, 0);
+            //VideoCapture.SetCaptureProperty(CapProp.AutoExposure, 0);
+            //VideoCapture.SetCaptureProperty(CapProp.Contrast, 20);
         }
 
         private void ProcessFrame(object sender, EventArgs e)
@@ -130,28 +129,32 @@ namespace EWU_TEALS_Draw
                 Mat videoFrame = VideoCapture.QueryFrame(); // If not managed, video frame causes .2mb/s Loss, does not get cleaned up by GC. Must manually dispose.
                 CvInvoke.Flip(videoFrame, videoFrame, FlipType.Horizontal);
                 ImageBox_VideoCapture.Image = videoFrame;
-                VideoFrames.Enqueue(videoFrame); // Add Video Frames to a queue to be disposed when NOT in use
+                DisposableQueue.Enqueue(videoFrame); // Add Video Frames to a queue to be disposed when NOT in use
 
-                Mat blueThreshImage = DetectColor(videoFrame, BlueHsvMin, BlueHsvMax, BlueDrawColor, BlueLastPosition, "Blue");
-                Mat greenThreshImage = DetectColor(videoFrame, GreenHsvMin, GreenHsvMax, GreenDrawColor, GreenLastPosition, "Green");
-                Mat yellowThreshImage = DetectColor(videoFrame, YellowHsvMin, YellowHsvMax, YellowDrawColor, YellowLastPosition, "Yellow");
-                Mat orangeThreshImage = DetectColor(videoFrame, OrangeHsvMin, OrangeHsvMax, OrangeDrawColor, OrangeLastPosition, "Orange");
-                Mat purpleThreshImage = DetectColor(videoFrame, PurpleHsvMin, PurpleHsvMax, PurpleDrawColor, PurpleLastPosition, "Purple");
-                Mat redThreshImage_Low = DetectColor(videoFrame, RedHsvMin_Low, RedHsvMax_Low, RedDrawColor, RedLastPosition, "Red");
+                //Mat redThreshImage_Low = DetectColor(videoFrame, RedHsvMin_Low, RedHsvMax_Low, RedDrawColor, RedLastPosition, "Red");
                 Mat redThreshImage_High = DetectColor(videoFrame, RedHsvMin_High, RedHsvMax_High, RedDrawColor, RedLastPosition, "Red");
+                //Mat orangeThreshImage = DetectColor(videoFrame, OrangeHsvMin, OrangeHsvMax, OrangeDrawColor, OrangeLastPosition, "Orange");
+                Mat yellowThreshImage = DetectColor(videoFrame, YellowHsvMin, YellowHsvMax, YellowDrawColor, YellowLastPosition, "Yellow");
+                Mat greenThreshImage = DetectColor(videoFrame, GreenHsvMin, GreenHsvMax, GreenDrawColor, GreenLastPosition, "Green");
+                Mat blueThreshImage = DetectColor(videoFrame, BlueHsvMin, BlueHsvMax, BlueDrawColor, BlueLastPosition, "Blue");
+                //Mat purpleThreshImage = DetectColor(videoFrame, PurpleHsvMin, PurpleHsvMax, PurpleDrawColor, PurpleLastPosition, "Purple");
 
-                CvInvoke.Add(blueThreshImage, greenThreshImage, CombinedThreshImage);
-                CvInvoke.Add(yellowThreshImage, CombinedThreshImage, CombinedThreshImage);
-                CvInvoke.Add(orangeThreshImage, CombinedThreshImage, CombinedThreshImage);
-                CvInvoke.Add(purpleThreshImage, CombinedThreshImage, CombinedThreshImage);
-                CvInvoke.Add(redThreshImage_Low, CombinedThreshImage, CombinedThreshImage);
-                CvInvoke.Add(redThreshImage_High, CombinedThreshImage, CombinedThreshImage);
+                Mat combinedThreshImage = Mat.Zeros(videoFrame.Rows, videoFrame.Cols, DepthType.Cv8U, 1);
+                DisposableQueue.Enqueue(combinedThreshImage);
+                //CvInvoke.Add(redThreshImage_Low, combinedThreshImage, combinedThreshImage);
+                CvInvoke.Add(redThreshImage_High, combinedThreshImage, combinedThreshImage);
+                //CvInvoke.Add(orangeThreshImage, combinedThreshImage, combinedThreshImage);
+                CvInvoke.Add(yellowThreshImage, combinedThreshImage, combinedThreshImage);
+                CvInvoke.Add(greenThreshImage, combinedThreshImage, combinedThreshImage);
+                CvInvoke.Add(blueThreshImage, combinedThreshImage, combinedThreshImage);
+                //CvInvoke.Add(purpleThreshImage, combinedThreshImage, combinedThreshImage);
 
-                ImageBox_VideoCapture_Gray.Image = CombinedThreshImage;
-                
-                if (VideoFrames.Count > 5)
+                ImageBox_VideoCapture_Gray.Image = combinedThreshImage;
+
+                if (DisposableQueue.Count > 8)
                 {
-                    VideoFrames.Dequeue().Dispose();
+                    DisposableQueue.Dequeue().Dispose();
+                    DisposableQueue.Dequeue().Dispose();
                 }
             }
         }
@@ -165,40 +168,56 @@ namespace EWU_TEALS_Draw
             // Convert pixels to white that are in specified color range, black otherwise, save to thresh_image
             CvInvoke.InRange(HsvImage_Temp, hsvThreshMin, hsvThreshMax, ThreshImage_Temp);
 
-            // Find average of white pixels
-            //Mat Points = new Mat(inputImage.Size, DepthType.Cv8U, 1);
-            CvInvoke.FindNonZero(ThreshImage_Temp, Points);
-
-            // An alternative approach to averaging would be to use the K-means 
-            // algorithm to find clusters, since average is significantly influenced by outliers.
-            MCvScalar avg = CvInvoke.Mean(Points);
-            AveragePoint.X = (int)avg.V0;
-            AveragePoint.Y = (int)avg.V1;
-
-            int sumWhitePixels = CvInvoke.CountNonZero(ThreshImage_Temp);
-
-            // Now we check if there are more than x pixels of detected color, since we don't want to draw
-            // if all we detect is noise.
-            if (sumWhitePixels > 300)
+            // Get contours of thresh image
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
             {
-                // Draw circle on camera feed
-                CvInvoke.Circle(inputImage, AveragePoint, 5, drawColor, 2);
+                //Mat hierarchy = new Mat(); Use this if need hierarchy parameter in FindContours
+                CvInvoke.FindContours(ThreshImage_Temp, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
 
-                // Draw on canvas
-                AveragePoint = ScaleToCanvas(AveragePoint);
+                // Find largest contour
+                double maxArea = 0;
+                int maxContourIndex = -1;
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    using (VectorOfPoint contour = contours[i])
+                    {
+                        double area = CvInvoke.ContourArea(contour);
+                        if (area > maxArea)
+                        {
+                            maxArea = area;
+                            maxContourIndex = i;
+                        }
+                    }
+                }
 
-                int width = GetWidthBySpeed(thisColorLastPosition, AveragePoint);
-                DrawLineTo(AveragePoint, drawColor, thisColorLastPosition, width);
+                // If at least one contour was found and its area is at least 300 pixels
+                if (maxContourIndex >= 0 && maxArea >= 1000)
+                {
+                    // Draw the contour in white
+                    CvInvoke.DrawContours(inputImage, contours, maxContourIndex, new MCvScalar(255, 255, 255), 2);
 
-                UpdateColorLastPosition(color, AveragePoint.X, AveragePoint.Y);
+                    // Get the contour center of mass
+                    MCvMoments moments = CvInvoke.Moments(contours[maxContourIndex]);
+                    Point contourCenter = new Point(
+                        (int)(moments.M10 / moments.M00),
+                        (int)(moments.M01 / moments.M00));
+
+                    // Draw the contour center on video feed
+                    CvInvoke.Circle(inputImage, contourCenter, 5, new MCvScalar(255, 255, 255), 2);
+
+                    // Draw on canvas using contour center
+                    contourCenter = ScaleToCanvas(contourCenter);
+                    int width = GetWidthBySpeed(thisColorLastPosition, contourCenter);
+                    DrawLineTo(contourCenter, drawColor, thisColorLastPosition, width);
+
+                    UpdateColorLastPosition(color, contourCenter.X, contourCenter.Y);
+                }
+                else
+                {
+                    UpdateColorLastPosition(color, 0, 0);
+                }
             }
-            // If not enough pixels to count as an object, reset lastColorPosition to 0 so it will be 
-            // updated when we do find it.
-            else
-            {
-                UpdateColorLastPosition(color, 0, 0);
-            }
-
+            
             return ThreshImage_Temp;
         }
 
@@ -282,7 +301,7 @@ namespace EWU_TEALS_Draw
 
             double travelDistance = Math.Sqrt(dx * dx + dy * dy);
             double speed = travelDistance / (1000 / FPS); // Speed as a ratio of pixels/frame length in ms
-            double maxAssumedSpeed = .8; // found this number through testing...
+            double maxAssumedSpeed = 2; // found this number through testing...
             double speedRatio = speed / maxAssumedSpeed;
 
             if (speedRatio > 1.0) speedRatio = 1.0;
@@ -334,9 +353,9 @@ namespace EWU_TEALS_Draw
                 Disposables = null;
             }
 
-            if (VideoFrames != null)
+            if (DisposableQueue != null)
             {
-                foreach (IDisposable disposable in VideoFrames)
+                foreach (IDisposable disposable in DisposableQueue)
                 {
                     if (disposable != null) disposable.Dispose();
                 }
