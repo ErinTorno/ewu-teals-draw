9259db5f9d23a921ed842a7903a5455a18d90468
Tuan Dang <tdang6@outlook.com>
2019-03-19 14:08:19 -0700

    Adding banner containing EWU logo and dock to the top

--- EWU_TEALS_Draw/EWU_TEALS_Draw/MainForm.Designer.cs
+++ EWU_TEALS_Draw/EWU_TEALS_Draw/MainForm.Designer.cs
@@ -29,55 +29,25 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.ImageBox_VideoCapture = new Emgu.CV.UI.ImageBox();
-            this.ImageBox_VideoCapture_Gray = new Emgu.CV.UI.ImageBox();
-            this.ImageBox_Drawing = new Emgu.CV.UI.ImageBox();
             this.btnPlay = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnReset = new System.Windows.Forms.Button();
             this.tableLayoutPanel_Drawing = new System.Windows.Forms.TableLayoutPanel();
+            this.ImageBox_Drawing = new Emgu.CV.UI.ImageBox();
             this.tableLayoutPanel_VideoFeeds = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel_Buttons = new System.Windows.Forms.TableLayoutPanel();
             this.btnEdit = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.ImageBox_VideoCapture)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ImageBox_VideoCapture_Gray)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ImageBox_Drawing)).BeginInit();
+            this.ImageBox_VideoCapture = new Emgu.CV.UI.ImageBox();
+            this.ImageBox_VideoCapture_Gray = new Emgu.CV.UI.ImageBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel_Drawing.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox_Drawing)).BeginInit();
             this.tableLayoutPanel_VideoFeeds.SuspendLayout();
             this.tableLayoutPanel_Buttons.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox_VideoCapture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox_VideoCapture_Gray)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // ImageBox_VideoCapture
-            // 
-            this.ImageBox_VideoCapture.Location = new System.Drawing.Point(5, 100);
-            this.ImageBox_VideoCapture.Margin = new System.Windows.Forms.Padding(5, 100, 5, 0);
-            this.ImageBox_VideoCapture.MaximumSize = new System.Drawing.Size(320, 180);
-            this.ImageBox_VideoCapture.Name = "ImageBox_VideoCapture";
-            this.ImageBox_VideoCapture.Size = new System.Drawing.Size(320, 180);
-            this.ImageBox_VideoCapture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.ImageBox_VideoCapture.TabIndex = 2;
-            this.ImageBox_VideoCapture.TabStop = false;
-            // 
-            // ImageBox_VideoCapture_Gray
-            // 
-            this.ImageBox_VideoCapture_Gray.Location = new System.Drawing.Point(5, 285);
-            this.ImageBox_VideoCapture_Gray.Margin = new System.Windows.Forms.Padding(5, 5, 5, 0);
-            this.ImageBox_VideoCapture_Gray.MaximumSize = new System.Drawing.Size(320, 180);
-            this.ImageBox_VideoCapture_Gray.Name = "ImageBox_VideoCapture_Gray";
-            this.ImageBox_VideoCapture_Gray.Size = new System.Drawing.Size(320, 180);
-            this.ImageBox_VideoCapture_Gray.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.ImageBox_VideoCapture_Gray.TabIndex = 2;
-            this.ImageBox_VideoCapture_Gray.TabStop = false;
-            // 
-            // ImageBox_Drawing
-            // 
-            this.ImageBox_Drawing.Location = new System.Drawing.Point(0, 100);
-            this.ImageBox_Drawing.Margin = new System.Windows.Forms.Padding(0, 100, 0, 0);
-            this.ImageBox_Drawing.Name = "ImageBox_Drawing";
-            this.ImageBox_Drawing.Size = new System.Drawing.Size(480, 270);
-            this.ImageBox_Drawing.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.ImageBox_Drawing.TabIndex = 3;
-            this.ImageBox_Drawing.TabStop = false;
             // 
             // btnPlay
             // 
@@ -85,7 +55,7 @@
             this.btnPlay.Location = new System.Drawing.Point(0, 0);
             this.btnPlay.Margin = new System.Windows.Forms.Padding(0);
             this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(300, 35);
+            this.btnPlay.Size = new System.Drawing.Size(225, 28);
             this.btnPlay.TabIndex = 4;
             this.btnPlay.Text = "Play/Pause";
             this.btnPlay.UseVisualStyleBackColor = true;
@@ -94,10 +64,10 @@
             // btnExit
             // 
             this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.Location = new System.Drawing.Point(0, 70);
+            this.btnExit.Location = new System.Drawing.Point(0, 56);
             this.btnExit.Margin = new System.Windows.Forms.Padding(0);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(300, 35);
+            this.btnExit.Size = new System.Drawing.Size(225, 28);
             this.btnExit.TabIndex = 7;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -106,10 +76,10 @@
             // btnReset
             // 
             this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReset.Location = new System.Drawing.Point(0, 35);
+            this.btnReset.Location = new System.Drawing.Point(0, 28);
             this.btnReset.Margin = new System.Windows.Forms.Padding(0);
             this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(300, 35);
+            this.btnReset.Size = new System.Drawing.Size(225, 28);
             this.btnReset.TabIndex = 6;
             this.btnReset.Text = "Clear Canvas";
             this.btnReset.UseVisualStyleBackColor = true;
@@ -119,18 +89,30 @@
             // 
             this.tableLayoutPanel_Drawing.AutoScroll = true;
             this.tableLayoutPanel_Drawing.AutoSize = true;
+            this.tableLayoutPanel_Drawing.BackColor = System.Drawing.Color.Transparent;
             this.tableLayoutPanel_Drawing.ColumnCount = 1;
             this.tableLayoutPanel_Drawing.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel_Drawing.Controls.Add(this.ImageBox_Drawing, 0, 0);
             this.tableLayoutPanel_Drawing.Dock = System.Windows.Forms.DockStyle.Left;
             this.tableLayoutPanel_Drawing.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel_Drawing.Margin = new System.Windows.Forms.Padding(0);
-            this.tableLayoutPanel_Drawing.MaximumSize = new System.Drawing.Size(1610, 0);
+            this.tableLayoutPanel_Drawing.MaximumSize = new System.Drawing.Size(1208, 0);
             this.tableLayoutPanel_Drawing.Name = "tableLayoutPanel_Drawing";
             this.tableLayoutPanel_Drawing.RowCount = 1;
             this.tableLayoutPanel_Drawing.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel_Drawing.Size = new System.Drawing.Size(480, 673);
+            this.tableLayoutPanel_Drawing.Size = new System.Drawing.Size(480, 547);
             this.tableLayoutPanel_Drawing.TabIndex = 8;
+            // 
+            // ImageBox_Drawing
+            // 
+            this.ImageBox_Drawing.Dock = System.Windows.Forms.DockStyle.Left;
+            this.ImageBox_Drawing.Location = new System.Drawing.Point(0, 81);
+            this.ImageBox_Drawing.Margin = new System.Windows.Forms.Padding(0, 81, 0, 0);
+            this.ImageBox_Drawing.Name = "ImageBox_Drawing";
+            this.ImageBox_Drawing.Size = new System.Drawing.Size(480, 466);
+            this.ImageBox_Drawing.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ImageBox_Drawing.TabIndex = 3;
+            this.ImageBox_Drawing.TabStop = false;
             // 
             // tableLayoutPanel_VideoFeeds
             // 
@@ -142,16 +124,16 @@
             this.tableLayoutPanel_VideoFeeds.Controls.Add(this.ImageBox_VideoCapture, 0, 0);
             this.tableLayoutPanel_VideoFeeds.Controls.Add(this.ImageBox_VideoCapture_Gray, 0, 1);
             this.tableLayoutPanel_VideoFeeds.Dock = System.Windows.Forms.DockStyle.Right;
-            this.tableLayoutPanel_VideoFeeds.Location = new System.Drawing.Point(932, 0);
+            this.tableLayoutPanel_VideoFeeds.Location = new System.Drawing.Point(618, 0);
             this.tableLayoutPanel_VideoFeeds.Margin = new System.Windows.Forms.Padding(0);
-            this.tableLayoutPanel_VideoFeeds.MinimumSize = new System.Drawing.Size(320, 0);
+            this.tableLayoutPanel_VideoFeeds.MinimumSize = new System.Drawing.Size(240, 0);
             this.tableLayoutPanel_VideoFeeds.Name = "tableLayoutPanel_VideoFeeds";
             this.tableLayoutPanel_VideoFeeds.RowCount = 3;
             this.tableLayoutPanel_VideoFeeds.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel_VideoFeeds.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel_VideoFeeds.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel_VideoFeeds.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel_VideoFeeds.Size = new System.Drawing.Size(330, 673);
+            this.tableLayoutPanel_VideoFeeds.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 16F));
+            this.tableLayoutPanel_VideoFeeds.Size = new System.Drawing.Size(328, 547);
             this.tableLayoutPanel_VideoFeeds.TabIndex = 9;
             // 
             // tableLayoutPanel_Buttons
@@ -165,49 +147,90 @@
             this.tableLayoutPanel_Buttons.Controls.Add(this.btnEdit, 0, 3);
             this.tableLayoutPanel_Buttons.Controls.Add(this.btnExit, 0, 2);
             this.tableLayoutPanel_Buttons.Controls.Add(this.btnReset, 0, 1);
-            this.tableLayoutPanel_Buttons.Location = new System.Drawing.Point(5, 470);
-            this.tableLayoutPanel_Buttons.Margin = new System.Windows.Forms.Padding(5, 5, 0, 0);
+            this.tableLayoutPanel_Buttons.Location = new System.Drawing.Point(4, 449);
+            this.tableLayoutPanel_Buttons.Margin = new System.Windows.Forms.Padding(4, 4, 0, 0);
             this.tableLayoutPanel_Buttons.Name = "tableLayoutPanel_Buttons";
             this.tableLayoutPanel_Buttons.RowCount = 4;
             this.tableLayoutPanel_Buttons.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel_Buttons.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel_Buttons.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel_Buttons.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel_Buttons.Size = new System.Drawing.Size(325, 140);
+            this.tableLayoutPanel_Buttons.Size = new System.Drawing.Size(324, 112);
             this.tableLayoutPanel_Buttons.TabIndex = 10;
             // 
             // btnEdit
             // 
             this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEdit.Location = new System.Drawing.Point(0, 105);
+            this.btnEdit.Location = new System.Drawing.Point(0, 84);
             this.btnEdit.Margin = new System.Windows.Forms.Padding(0);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(300, 35);
+            this.btnEdit.Size = new System.Drawing.Size(225, 28);
             this.btnEdit.TabIndex = 5;
             this.btnEdit.Text = "Edit Detection";
             this.btnEdit.UseVisualStyleBackColor = true;
-            this.btnEdit.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // ImageBox_VideoCapture
+            // 
+            this.ImageBox_VideoCapture.Location = new System.Drawing.Point(4, 81);
+            this.ImageBox_VideoCapture.Margin = new System.Windows.Forms.Padding(4, 81, 4, 0);
+            this.ImageBox_VideoCapture.MaximumSize = new System.Drawing.Size(320, 180);
+            this.ImageBox_VideoCapture.Name = "ImageBox_VideoCapture";
+            this.ImageBox_VideoCapture.Size = new System.Drawing.Size(320, 180);
+            this.ImageBox_VideoCapture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ImageBox_VideoCapture.TabIndex = 2;
+            this.ImageBox_VideoCapture.TabStop = false;
+            // 
+            // ImageBox_VideoCapture_Gray
+            // 
+            this.ImageBox_VideoCapture_Gray.Location = new System.Drawing.Point(4, 265);
+            this.ImageBox_VideoCapture_Gray.Margin = new System.Windows.Forms.Padding(4, 4, 4, 0);
+            this.ImageBox_VideoCapture_Gray.MaximumSize = new System.Drawing.Size(320, 180);
+            this.ImageBox_VideoCapture_Gray.Name = "ImageBox_VideoCapture_Gray";
+            this.ImageBox_VideoCapture_Gray.Size = new System.Drawing.Size(320, 180);
+            this.ImageBox_VideoCapture_Gray.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ImageBox_VideoCapture_Gray.TabIndex = 2;
+            this.ImageBox_VideoCapture_Gray.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.pictureBox1.ErrorImage = global::EWU_TEALS_Draw.Properties.Resources.logo_uh_color;
+            this.pictureBox1.Image = global::EWU_TEALS_Draw.Properties.Resources.logo_uh_color;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(939, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(110)))), ((int)(((byte)(130)))));
-            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(946, 547);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.tableLayoutPanel_Drawing);
             this.Controls.Add(this.tableLayoutPanel_VideoFeeds);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainForm";
             this.Text = "EWU TEALS Draw";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
-            ((System.ComponentModel.ISupportInitialize)(this.ImageBox_VideoCapture)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ImageBox_VideoCapture_Gray)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ImageBox_Drawing)).EndInit();
             this.tableLayoutPanel_Drawing.ResumeLayout(false);
             this.tableLayoutPanel_Drawing.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox_Drawing)).EndInit();
             this.tableLayoutPanel_VideoFeeds.ResumeLayout(false);
             this.tableLayoutPanel_VideoFeeds.PerformLayout();
             this.tableLayoutPanel_Buttons.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox_VideoCapture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox_VideoCapture_Gray)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -225,6 +248,7 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_VideoFeeds;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_Buttons;
         private System.Windows.Forms.Button btnEdit;
+        public System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
